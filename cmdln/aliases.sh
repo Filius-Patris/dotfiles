55423fe5056104cc2e472a7f54e1c82ca4275710
Filius Patris <filiuspatrisapps@gmail.com>
2019-01-19 20:40:52 +0100

    improved mute shortcut

--- cmdln/aliases.sh
+++ cmdln/aliases.sh
@@ -1,4 +1,4 @@
-alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color="never" percentage;
-        upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color="never" time'
+alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color=never percentage;
+        upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color=never time'
 
-alias mute='amixer set Master toggle'
+alias mute="amixer set Master toggle | awk '/\[o.*\]/{print \$6}'"
