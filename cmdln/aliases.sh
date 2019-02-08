f05ea23f448af3c53aa81a0c9dcbf1f6dd4012f0
Filius Patris <filiuspatrisapps@gmail.com>
2019-02-08 20:45:52 +0100

    Changed name of hdmi shortcuts

--- cmdln/aliases.sh
+++ cmdln/aliases.sh
@@ -3,5 +3,5 @@ alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color
 
 alias mute="amixer set Master toggle | awk '/\[o.*\]/{print \$6}'"
 
-alias hdmiON='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --right-of "eDP-1" --auto'
-alias hdmiOFF='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --off'
+alias hdmi-on='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --right-of "eDP-1" --auto'
+alias hdmi-off='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --off'
