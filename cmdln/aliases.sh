17479929a4a5287df94d2218858206065580e7e2
Filius Patris <filiuspatrisapps@gmail.com>
2019-02-08 13:10:47 +0100

    Added xrandr shortcuts for hdmi second display

--- cmdln/aliases.sh
+++ cmdln/aliases.sh
@@ -2,3 +2,6 @@ alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color
         upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --color=never time'
 
 alias mute="amixer set Master toggle | awk '/\[o.*\]/{print \$6}'"
+
+alias hdmiON='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --right-of "eDP-1" --auto'
+alias hdmiOFF='xrandr --output "eDP-1" --primary --auto --output "HDMI-1" --off'
