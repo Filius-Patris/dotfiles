47d19cde9bfa9e1e75d9d470bc4e89aeb32517f1
Filius Patris <filiuspatrisapps@gmail.com>
2019-06-29 14:24:36 +0200

    bugfix with unescaped characters

--- scripts/randomBG.sh
+++ scripts/randomBG.sh
@@ -4,7 +4,7 @@ DIR=~/Files/Wallpapers/current
 
 while true
 do
-		currentPic=$DIR/`ls $DIR | shuf | head -n1`
+		currentPic=$DIR/`ls --escape $DIR | shuf | head -n1`
 		echo "`date`	$currentPic"
 		feh --bg-scale $currentPic
 		sleep $INTERVAL
